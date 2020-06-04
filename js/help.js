60a5e9eabb7e9c10b99dcd626ba421558ece3058
rafael <rafaelhss@gmail.com>
2020-06-04 19:28:22 -0300

    boa versao

--- js/help.js
+++ js/help.js
@@ -23,7 +23,7 @@ function switchon(tagname){
         
 }
 function highlight(tagname, showhelp){
-    changeAllOpacity("0.65");
+    changeAllOpacity("0.6");
     switchon(tagname);    
     if(showhelp == "up"){
         switchon("help")
@@ -35,8 +35,8 @@ function highlight(tagname, showhelp){
 }
     
 var helps =[
-    {"helppos":"down", "tag":"holder", "text":"Nessa area voce ve atributos do jogador que esta com a bola. A cor indica o time. Azul significa que a bola esta com voce e os comandos desenhados no canvas serao executados."},
     {"helppos":"down", "tag":"field", "text":"Nessa area voce ve o campo. o setor destacado eh onde esta a bola. Se um jogador for driblado fica fora de jogo por alguns segundos."},
+    {"helppos":"down", "tag":"holder", "text":"Nessa area voce ve atributos do jogador que esta com a bola. A cor indica o time. Azul significa que a bola esta com voce e os comandos desenhados no canvas serao executados."},
     {"helppos":"down", "tag":"chances", "text":"Nessa area voce ve os tres movimentos com mais chance de dar certo com base nas habilidades do seu jogador e dos adversarios envolvidos."},
     {"helppos":"down", "tag":"terminal", "text":"Nessa area aparecem os lances da partida."},
     {"helppos":"up", "tag":"canvas", "text":"Aqui eh o canvas, onde voce deve desenhar os comandos para passe (8 direcoes), dibre ou chute a gol. <br>Dibre:<img src=\"img/dibre.png\"><br>Chute:<img src=\"img/shot.png\"><br>Passe:<img src=\"img/right.png\"><img src=\"img/leftup.png\"><img src=\"img/leftdown.png\">..."}
